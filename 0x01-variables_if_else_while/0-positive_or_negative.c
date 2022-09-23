3cf9781a0e3d350338677ef96914a799b44d695f
hamzaabdullahi <hamzaabdullahiyakub@gmail.com>
2022-09-23 06:32:32 -0700

    is negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	}
-	 else if (n == 0)
-	 {
+	} else if (n == 0)
+	{
 		 printf("%d is zero\n", n);
 	 } else
 	 {
 		 printf("%d is positive\n", n);
 	 }
+
 	 return (0);
 }	  
